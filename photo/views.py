4bd3b537fc36296bf874f7c0ad426bf833844c24
logistex <logistex@naver.com>
2020-05-17 23:04:09 +0900

    merged three, pretty

--- photo/views.py
+++ photo/views.py
@@ -1,15 +1,21 @@
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
 from .models import Photo
 
 
-@login_required
-def photo_list(request):
-    photos = Photo.objects.all()
-    return render(request, 'photo/list.html', {'photos': photos})
+# @login_required
+# def photo_list(request):
+#     photos = Photo.objects.all()
+#     return render(request, 'photo/list.html', {'photos': photos})
+class PhotoListView(LoginRequiredMixin, ListView):
+    model = Photo
+    paginate_by = 3
+    template_name = 'photo/list.html'
 
 
 class PhotoUploadView(LoginRequiredMixin, CreateView):
@@ -39,3 +45,8 @@ class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/update.html'
+
+
+class PhotoDetailView(LoginRequiredMixin, DetailView):
+    model = Photo
+    template_name = 'photo/detail.html'
